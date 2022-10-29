dcce92e7b38fccfdfd3e3672fc1f64310f7251ae
Santiag Lopera <santiagoln09@gmail.com>
2022-10-28 22:56:11 -0500

    challenge CSV files and frist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Completions } from './Completions';
 
 function App() {
   return (
     <div className="App">
+      <Completions />
     </div>
   );
 }
---
+++ src/Completions.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Completions = () => {
+  return (
+    <div>Completions</div>
+  )
+}
