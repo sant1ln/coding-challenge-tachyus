50b88493a817df9d17aa0967a48fb8fd02c21cbc
Santiag Lopera <santiagoln09@gmail.com>
2022-10-30 16:22:55 -0500

    Show table with data

--- src/Completions.jsx
+++ src/Completions.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { usePapaParse } from 'react-papaparse';
+import productionFile from './data/completions.csv'
+import './Completions.css'
 
 export const Completions = () => {
+
+  const [completionsDataHeader, setCompletionsDataHeader] = useState([]);
+  const [completionsDataBody, setCompletionsDataBody] = useState([]);
+  const { readRemoteFile } = usePapaParse();
+
+  useEffect(() => {
+    readRemoteFile(productionFile, {
+      complete: (results) => {
+        setCompletionsDataHeader(results.data[0]);
+        setCompletionsDataBody(results.data.slice(1, 50))
+        console.log(results.data.slice(1, 50))
+      },
+    });
+  }, [])
+
+
+
   return (
-    <div>Completions</div>
+    <>
+      <div>Completions</div>
+      <table cellSpacing="0">
+        <thead>
+          <tr>
+            {
+              completionsDataHeader.map((data, index) => (
+                <th key={index}>{data}</th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            completionsDataBody.map((data, index) => (
+              <tr key={index}>
+                {data.map((info,index) => (
+                  <td key={index}>{info}</td>
+                ))}
+              </tr>
+            ))
+          }
+        </tbody>
+      </table></>
   )
 }
+/* https://github.com/Bunlong/react-papaparse/blob/v4.0.0/examples/readRemoteFile.tsx */
